fad6cd4f5bbf640feb681318a9769336404e4ba5
Yevhenii Lichman <zhekalichm4n@icloud.com>
2023-09-25 19:31:04 +0300

    update script

---
+++ forrest.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+    var text1 = document.getElementById("massBody");
+    var button = document.getElementById("mainButton");
+    var text2 = document.getElementById("heightBody");
+    var result = document.getElementById("result__item");
+    button.onclick = function (event) {
+        result.innerHTML = ''
+        var mass = text1.value;
+        var height = text2.value / 100;
+        normal = mass / (height * height)
+        if (!normal) {
+            result.innerHTML += 'Спочатку заповніть відповідні поля.'
+        }
+        else {
+            result.innerHTML = Math.round(normal)
+            if (normal < 25) {
+                result.innerHTML += ' - у вас нормальна вага'
+            }
+            else {
+                if (normal > 30) {
+                    result.innerHTML += ' - у вас ожиріння'
+                }
+                else {
+                    result.innerHTML += ' - вважається надмірною масою тіла'
+                }
+            }
+        }
+    };
+};
